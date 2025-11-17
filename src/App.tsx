8e406eb68a6d6111a6f904350fa3f33061a7f166
im-ivan <ivanhfc00@gmail.com>
2025-11-17 13:28:46 -0300

    ft: navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css'
+import { Header } from './components/Header'
 
 function App() {
 
   return (
     <>
+      <div className="main-container bg-[#F2E8DF]">
+        <Header></Header>
+      </div>
     </>
   )
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+export const Header = () => {
+    return <>
+        <header className="bg-[#D9BB84] p-3">
+            <nav className="block border border-[#592202] py-5">
+                <ul className="flex justify-end space-x-3">
+                    <li><a href="" className="p-5 hover:text-[#BF532C]">Home</a></li>
+                    <li><a href="" className="p-5 hover:text-[#BF532C]">Catálogo</a></li>
+                    <li><a href="" className="p-5 hover:text-[#BF532C]">Cesta de Análise</a></li>
+                </ul>
+            </nav>
+        </header>
+    </>
+}
