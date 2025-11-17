1bbba28a664beb75a382754a6077a3a2580f5ae0
im-ivan <ivanhfc00@gmail.com>
2025-11-17 15:35:29 -0300

    ft: footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+export const Footer = () => {
+    return (
+        <>
+            <footer className="bg-[#D9BB84] text-center py-4 mt-10">
+                <p className="text-sm text-[#592202]">© 2024 Saliri Corp. Nenhum  direito reservado.</p>
+            </footer>
+        </>
+    )
+}
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom"
 import { Header } from "./Header"
+import { Footer } from "./Footer"
 
 export const Layout = () => {
     return (
-        <>
+        <div className="min-h-screen flex flex-col">
             <Header />
-            <main className="mt-4">
+            <main className="grow mt-4">
                 <Outlet />
             </main>
-        </>
+            <Footer></Footer>
+        </div>
     )
 }
