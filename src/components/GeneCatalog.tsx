3d214a2ec40855a2cbcf8f9e9935653281fb3e3f
im-ivan <ivanhfc00@gmail.com>
2025-11-18 11:20:28 -0300

    ft: impl catalog filters

--- src/components/GeneCatalog.tsx
+++ src/components/GeneCatalog.tsx
@@ -4,15 +4,37 @@ import geneData from "../assets/bio-data-db.json"
 import type { Gene } from "../types/Gene"
 import { GeneCard } from "./GeneCard"
 
+interface FilterFields {
+    name: boolean;
+    id: boolean;
+    function: boolean;
+}
 export const GeneCatalog = () => {
-
     const genesList: Gene[] = geneData
+    const [filters, setFilters] = useState<FilterFields>({
+        name: true,
+        id: true,
+        function: true
+    })
+
+    const handleFilterChange = (field: keyof FilterFields) => {
+        setFilters(prevFilters => ({
+            ...prevFilters,
+            [field]: !prevFilters[field]
+        }))
+    }
+
     const [search, setSearch] = useState<string>("");
-    const filteredGenes = genesList.filter(gene =>
-        gene.name.toLowerCase().includes(search.toLowerCase()) ||
-        gene.id.toString().includes(search) ||
-        gene.function.toLowerCase().includes(search.toLowerCase())
-    )
+    const filteredGenes = genesList.filter(gene => {
+        const query = search.toLowerCase();
+        const nameMatch = filters.name && gene.name.toLowerCase().includes(query)
+        const idMatch = filters.id && gene.id.toLowerCase().includes(query)
+        const functionMatch = filters.function && gene.function.toLowerCase().includes(query)
+        return nameMatch || idMatch || functionMatch
+        // gene.name.toLowerCase().includes(search.toLowerCase()) ||
+        //     gene.id.toString().includes(search) ||
+        //     gene.function.toLowerCase().includes(search.toLowerCase())
+    })
 
     return <>
         <div className="catalog-filter-area p-5">
@@ -22,6 +44,7 @@ export const GeneCatalog = () => {
                         className="w-full bg-transparent placeholder:text-slate-400 text-amber-700 text-sm border border-slate-200 rounded-md pl-3 pr-28 py-2 transition duration-300 ease focus:outline-none focus:border-amber-400 hover:border-amber-600 shadow-sm focus:shadow"
                         placeholder="Example: Protein"
                         onChange={e => setSearch(e.target.value)}
+                        value={search}
                     />
                     <button
                         className="absolute top-1 right-1 flex items-center rounded bg-amber-800 py-1 px-2.5 border border-transparent text-center text-sm text-white transition-all shadow-sm hover:shadow focus:bg-amber-700 focus:shadow-none active:bg-amber hover:bg-amber-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
@@ -30,22 +53,39 @@ export const GeneCatalog = () => {
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-4 h-4 mr-2">
                             <path fill-rule="evenodd" d="M10.5 3.75a6.75 6.75 0 1 0 0 13.5 6.75 6.75 0 0 0 0-13.5ZM2.25 10.5a8.25 8.25 0 1 1 14.59 5.28l4.69 4.69a.75.75 0 1 1-1.06 1.06l-4.69-4.69A8.25 8.25 0 0 1 2.25 10.5Z" clip-rule="evenodd" />
                         </svg>
-
                         Buscar
                     </button>
                 </div>
                 <div className="flex">
                     <div className="input-group flex items-center mx-3">
                         <label className="m-1" htmlFor="gene-name">Name</label>
-                        <input type="checkbox" className="m-1" name="gene-name" id="gene-name" />
+                        <input type="checkbox"
+                            className="m-1"
+                            name="gene-name"
+                            id="gene-name"
+                            checked={filters.name}
+                            onChange={() => handleFilterChange("name")}
+                        />
                     </div>
                     <div className="input-group flex items-center mx-3">
-                        <label className="m-1" htmlFor="gene-name">Id</label>
-                        <input type="checkbox" className="m-1" name="gene-name" id="gene-name" />
+                        <label className="m-1" htmlFor="gene-id">Id</label>
+                        <input type="checkbox"
+                            className="m-1"
+                            name="gene-id"
+                            id="gene-id"
+                            checked={filters.id}
+                            onChange={() => handleFilterChange("id")}
+                        />
                     </div>
                     <div className="input-group flex items-center mx-3">
-                        <label className="m-1" htmlFor="gene-name">Function</label>
-                        <input type="checkbox" className="m-1" name="gene-name" id="gene-name" />
+                        <label className="m-1" htmlFor="gene-function">Function</label>
+                        <input type="checkbox"
+                            className="m-1"
+                            name="gene-function"
+                            id="gene-function"
+                            checked={filters.function}
+                            onChange={() => handleFilterChange("function")}
+                        />
                     </div>
                 </div>
             </div>
@@ -58,6 +98,7 @@ export const GeneCatalog = () => {
                     )
                 })
             }
+            {filteredGenes.length === 0 && <p className="col-span-full text-center text-slate-500">Nenhum gene encontrado com os critérios de busca e filtro.</p>}
         </div>
     </>
 }
